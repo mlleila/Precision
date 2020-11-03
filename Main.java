0de4d8d293a1070e07675eebeeebff07fa516ff5
Leila Mamedova <m.lleila@bk.ru>
2020-11-03 20:34:00 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float regularBonus = 0.3F;
+        float specialBonus = 0.6F;
+        float totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
+}
